2fa5686d7edb776743a3ac79604089091e2d6768
Beno Suriano <bjsuriano@gmail.com>
2022-11-20 20:50:41 -0400

    Modify rules on the eslintrc.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,13 @@
-const express = require('express')
+import express from 'express'
 
 const app = express()
 const port = 9000
 
-let one: number = 1
-let two: number = 2
+const one = 1
+const two = 2
 
-app.get('/',(req:any,res:any) => res.send(`1 + 2 = ${one+two}`))
+// To notify that we're aware of this unused parameter, we can prefix the req param with an underscore.
+app.get('/',(_req,res) => res.send(`1 + 2 = ${one+two}`))
 
 app.listen(port)
 
