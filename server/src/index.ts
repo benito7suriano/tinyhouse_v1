8bf5c5af90e4aeb42a98bd5c72d2e3c935b8f631
Beno Suriano <bjsuriano@gmail.com>
2022-11-20 20:41:02 -0400

    Setup eslintrc and extend prettier.

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,8 +3,8 @@ const express = require('express')
 const app = express()
 const port = 9000
 
-let one = 1
-let two = 2
+let one: number = 1
+let two: number = 2
 
 app.get('/',(req:any,res:any) => res.send(`1 + 2 = ${one+two}`))
 
