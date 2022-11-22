7a05b729343147a63bd1c8c7a333b80ab6909140
Beno Suriano <bjsuriano@gmail.com>
2022-11-22 09:16:55 -0400

    DELETE a mock listing using post route on server.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,14 @@
 import express from 'express'
 
-import { listings } from './listings'
+import { listings, Listing } from './listings'
 
 const app = express()
 const port = 9000
 
 const one = 1
 const two = 2
+
+app.use(express.json())
 
 app.get('/',(_req,res) => res.send(`1 + 2 = ${one+two}`))
 
@@ -15,7 +17,16 @@ app.get('/listings', (_req,res) => {
   res.send(listings)
 })
 
+app.post('/delete-listing', (req,res) => {
+  const id:string = req.body.id
+  for(let i = 0; i < listings.length; i++) {
+    if(listings[i].id === id) {
+      return res.send(listings.splice(i,1))
+    }
+  }
 
+  return res.send('failed to delete listing')
+})
 
 app.listen(port, () => {
   console.log(`[app] : http://localhost:${port}`)
