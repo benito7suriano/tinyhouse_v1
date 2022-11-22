69d588c7e0ffd45d244fca035209a61f7bfde2da
Beno Suriano <bjsuriano@gmail.com>
2022-11-22 09:02:40 -0400

    GET mock listings from server.

--- server/build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const express = require('express');
-const app = express();
-const port = 9000;
-let one = 1;
-let two = 2;
-app.get('/', (req, res) => res.send(`1 + 2 = ${one + two}`));
-app.listen(port);
-console.log(`[app] : http://localhost:${port}`);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,22 @@
 import express from 'express'
 
+import { listings } from './listings'
+
 const app = express()
 const port = 9000
 
 const one = 1
 const two = 2
 
-// To notify that we're aware of this unused parameter, we can prefix the req param with an underscore.
 app.get('/',(_req,res) => res.send(`1 + 2 = ${one+two}`))
 
-app.listen(port)
+// To notify that we're aware of this unused parameter, we can prefix the req param with an underscore.
+app.get('/listings', (_req,res) => {
+  res.send(listings)
+})
 
-console.log(`[app] : http://localhost:${port}`)
+
+
+app.listen(port, () => {
+  console.log(`[app] : http://localhost:${port}`)
+})
