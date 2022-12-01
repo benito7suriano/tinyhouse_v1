3796a28f232aec7f09065723ecfc415cf07e5dfd
Beno Suriano <bjsuriano@gmail.com>
2022-12-01 12:58:21 -0400

    Await mongo client and return object with listings from db.

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -4,8 +4,16 @@ const user = 'beno7suriano'
 const password = ''
 const cluster = 'tinyhouse-v1.mmwkocb'
 
-const url = `mongodb+srv://beno7suriano:${password}@${cluster}.mongodb.net/?retryWrites=true&w=majority`
+const url = `mongodb+srv://${user}:${password}@${cluster}.mongodb.net/?retryWrites=true&w=majority`
 
-export const connectDatabase = () => {
-  const client = MongoClient.connect(url)
+export const connectDatabase = async () => {
+  const client = await MongoClient.connect(url, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+  })
+  const db = client.db('main')
+
+  return {
+    listings: db.collection('test_listings')
+  }
 }
