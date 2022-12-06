83e648ccb320dbc640150facd182d4a7d2fdfd9c
Beno Suriano <bjsuriano@gmail.com>
2022-12-06 18:10:14 -0400

    Update Query resolver to fetch from db.

--- server/src/graphql/resolvers.ts
+++ server/src/graphql/resolvers.ts
@@ -1,10 +1,11 @@
 import { listings } from "../listings";
 import { IResolvers } from "@graphql-tools/utils";
+import { Database } from "../lib/types";
 
 export const resolvers: IResolvers = {
   Query: {
-    listings: () => {
-      return listings
+    listings: async (_root:undefined, _args:Record<string, unknown>, { db }: {db: Database} ) => {
+      return await db.listings.find({}).toArray()
     }
   },
   Mutation: {
