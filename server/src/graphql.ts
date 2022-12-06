ada11fa47ba6ed27f85366f0f278f7795d02ccd2
Beno Suriano <bjsuriano@gmail.com>
2022-12-06 15:17:14 -0400

    Comment out graphql file.

--- server/src/graphql.ts
+++ server/src/graphql.ts
@@ -1,51 +1,51 @@
-import { GraphQLSchema, GraphQLString, GraphQLObjectType, GraphQLID, GraphQLInt, GraphQLNonNull, GraphQLList } from "graphql";
+// import { GraphQLSchema, GraphQLString, GraphQLObjectType, GraphQLID, GraphQLInt, GraphQLNonNull, GraphQLList } from "graphql";
 
-import { listings } from "./listings";
+// import { listings } from "./listings";
 
-const Listing = new GraphQLObjectType({
-  name: "Listing",
-  fields: {
-    id: { type: new GraphQLNonNull(GraphQLID) },
-    title: {type: new GraphQLNonNull(GraphQLString)},
-    image: {type: new GraphQLNonNull(GraphQLString)},
-    address: {type: new GraphQLNonNull(GraphQLString)},
-    price: {type: new GraphQLNonNull(GraphQLInt)},
-    numOfGuests: {type: new GraphQLNonNull(GraphQLInt)},
-    numOfBeds: {type: new GraphQLNonNull(GraphQLInt)},
-    numOfBaths: {type: new GraphQLNonNull(GraphQLInt)},
-    rating: {type: new GraphQLNonNull(GraphQLInt)},
-  }
-})
+// const Listing = new GraphQLObjectType({
+//   name: "Listing",
+//   fields: {
+//     id: { type: new GraphQLNonNull(GraphQLID) },
+//     title: {type: new GraphQLNonNull(GraphQLString)},
+//     image: {type: new GraphQLNonNull(GraphQLString)},
+//     address: {type: new GraphQLNonNull(GraphQLString)},
+//     price: {type: new GraphQLNonNull(GraphQLInt)},
+//     numOfGuests: {type: new GraphQLNonNull(GraphQLInt)},
+//     numOfBeds: {type: new GraphQLNonNull(GraphQLInt)},
+//     numOfBaths: {type: new GraphQLNonNull(GraphQLInt)},
+//     rating: {type: new GraphQLNonNull(GraphQLInt)},
+//   }
+// })
 
-const query = new GraphQLObjectType({
-  name: "Query",
-  fields: {
-    listings: {
-      type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(Listing))),
-      resolve: () => listings
-    }
-  }
-});
+// const query = new GraphQLObjectType({
+//   name: "Query",
+//   fields: {
+//     listings: {
+//       type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(Listing))),
+//       resolve: () => listings
+//     }
+//   }
+// });
 
-const mutation = new GraphQLObjectType({
-  name: "Mutation",
-  fields: {
-    deleteListing: {
-      type: new GraphQLNonNull(Listing),
-      args: {
-        id: {type: new GraphQLNonNull(GraphQLID)}
-      },
-      resolve: (_root, { id }) => {
-        for (let i = 0; i < listings.length; i++) {
-          if (listings[i].id === id) {
-            return listings.splice(i, 1)[0];
-          }
-        }
+// const mutation = new GraphQLObjectType({
+//   name: "Mutation",
+//   fields: {
+//     deleteListing: {
+//       type: new GraphQLNonNull(Listing),
+//       args: {
+//         id: {type: new GraphQLNonNull(GraphQLID)}
+//       },
+//       resolve: (_root, { id }) => {
+//         for (let i = 0; i < listings.length; i++) {
+//           if (listings[i].id === id) {
+//             return listings.splice(i, 1)[0];
+//           }
+//         }
 
-        throw new Error("failed to deleted listing");
-      }
-    }
-  }
-});
+//         throw new Error("failed to deleted listing");
+//       }
+//     }
+//   }
+// });
 
-export const schema = new GraphQLSchema({query,mutation})
+// export const schema = new GraphQLSchema({query,mutation})
