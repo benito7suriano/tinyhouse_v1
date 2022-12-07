21fb760c1f8928df26bfad0d76466af6dd473598
Beno Suriano <bjsuriano@gmail.com>
2022-12-07 10:34:59 -0400

    Process data from MongoDB successfully.

--- server/src/graphql/resolvers.ts
+++ server/src/graphql/resolvers.ts
@@ -4,12 +4,12 @@ import { ObjectId } from "mongodb";
 
 export const resolvers: IResolvers = {
   Query: {
-    listings: async (_root:undefined, _args:Record<string, unknown>, { db }: {db: Database} ) => {
+    listings: async (_root:undefined, _args:Record<string, unknown>, { db }: {db: Database} ):Promise<Listing[]> => {
       return await db.listings.find({}).toArray()
     }
   },
   Mutation: {
-    deleteListing: async (_root:undefined, { id }:{ id:string }, { db }:{ db: Database }) => {
+    deleteListing: async (_root:undefined, { id }:{ id:string }, { db }:{ db: Database }):Promise<Listing> => {
       const deletedListing = await db.listings.findOneAndDelete({
         _id: new ObjectId(id)
       })
