c406ecbb59003c84f75e1fcb6bb9d736d501cd07
Beno Suriano <bjsuriano@gmail.com>
2022-12-12 21:40:29 -0400

    Initialize Listings component and sections folder structure.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
+import { Listings } from './sections';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <h1>Hello world</h1>
+    <Listings />
   </React.StrictMode>
 );
 
---
+++ client/src/sections/Listings/Listings.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Listings = () => {
+  return (
+    <div>TinyHouse Listings</div>
+  )
+}
+
