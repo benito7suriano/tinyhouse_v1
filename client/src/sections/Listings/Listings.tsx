769a57509c286e469f94721549d7d5d92169f18f
Beno Suriano <bjsuriano@gmail.com>
2022-12-14 22:09:24 -0400

    Import client server and define LISTINGS object type in Listings component.

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,4 +1,21 @@
 import React from 'react'
+import { server } from '../../lib/api'
+
+const LISTINGS = `
+  query {
+    listings {
+      id
+      title
+      image
+      address
+      price
+      numOfGuests
+      numOfBeds
+      numOfBaths
+      rating
+    }
+  }
+`
 
 interface Props {
   title: string
