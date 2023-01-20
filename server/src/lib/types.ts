846ca58926a757bef72fa5f3395978b7d4fa4d48
Beno Suriano <bjsuriano@gmail.com>
2023-01-20 13:37:22 -0400

    Create Listing collection interface.

--- server/src/lib/types.ts
+++ server/src/lib/types.ts
@@ -1,11 +1,41 @@
 import { Collection, ObjectId } from 'mongodb'
 
+export interface BookingsIndexMonth {
+  [key: string]: boolean
+}
+
+export interface BookingsIndexYear {
+  [key: string]: BookingsIndexMonth
+}
+
+export interface BookingsIndex {
+  [key: string]: BookingsIndexYear
+}
+
 export interface Booking {
   _id: ObjectId
 }
 
+export enum ListingType {
+  Apartment = 'APARTMENT',
+  House = 'HOUSE',
+}
+
 export interface Listing {
   _id: ObjectId
+  title: string
+  description: string
+  image: string
+  host: string
+  type: ListingType
+  address: string
+  country: string
+  admin: string
+  city: string
+  bookings: ObjectId[]
+  bookingsIndex: BookingsIndex
+  price: number
+  numOfGuests: number
 }
 
 export interface User {
