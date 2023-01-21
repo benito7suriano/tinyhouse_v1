0d3e4c900ef0de79c892d9580c211184a539ca5f
Beno Suriano <bjsuriano@gmail.com>
2023-01-20 22:45:54 -0400

    Setup authUrl and logIn functionality for Google OAuth.

---
+++ server/src/lib/api/Google.ts
@@ -0,0 +1,32 @@
+import { google } from 'googleapis'
+
+const auth = new google.auth.OAuth2(
+  process.env.G_CLIENT_ID,
+  process.env.G_CLIENT_SECRET,
+  `${process.env.PUBLIC_URL}/login`
+)
+
+export const Google = {
+  authUrl: auth.generateAuthUrl({
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    access_type: 'online',
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.email",
+      "https://www.googleapis.com/auth/userinfo.profile"
+    ]
+  }),
+  logIn:async (code:string) => {
+    // make a request to Google using a 'code' arg to get a user's access token
+    const { tokens } = await auth.getToken(code)
+
+    auth.setCredentials(tokens)
+
+    // At this moment, we can now use the configured auth object to make a request to Google's People API to get the user information we'll need. To do so, we'll run the people() constructor from the imported google object.
+    const { data } = await google.people({ version: 'v1', auth }).people.get({
+      resourceName: 'people/me',
+      personFields: 'emailAddresses,names,photos'
+    })
+
+    return { user: data }
+  }
+}
