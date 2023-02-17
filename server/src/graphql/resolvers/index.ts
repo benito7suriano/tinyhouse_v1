bfbb5a8ab657b9f7bf97a2cbb5a0872302a2ebdd
Beno Suriano <bjsuriano@gmail.com>
2023-02-17 17:12:27 -0400

    Merge Listings and Bookings resolvers with the rest.

--- server/src/graphql/resolvers/index.ts
+++ server/src/graphql/resolvers/index.ts
@@ -2,5 +2,11 @@ import merge from 'lodash.merge'
 import { viewerResolvers } from './Viewer'
 import { userResolvers } from './User'
 import { listingResolvers } from './Listing'
+import { bookingResolvers } from './Booking'
 
-export const resolvers = merge(viewerResolvers, userResolvers, listingResolvers)
+export const resolvers = merge(
+  viewerResolvers,
+  userResolvers,
+  listingResolvers,
+  bookingResolvers,
+)
