29000ae87523387626f5aa1ae4b665db86b5e415
Beno Suriano <bjsuriano@gmail.com>
2023-02-17 16:21:13 -0400

    Create UserBookings element.

--- client/src/sections/User/components/UserBookings/index.tsx
+++ client/src/sections/User/components/UserBookings/index.tsx
@@ -10,11 +10,70 @@ interface Props {
   setBookingsPage: (page: number) => void
 }
 
+const { Paragraph, Text, Title } = Typography
+
 export const UserBookings = ({
   userBookings,
   bookingsPage,
   limit,
   setBookingsPage,
 }: Props) => {
-  return <div>UserBookings</div>
+  const total = userBookings ? userBookings.total : null
+  const result = userBookings ? userBookings.result : null
+
+  const userBookingsList = userBookings && (
+    <List
+      grid={{
+        gutter: 8,
+        xs: 1,
+        sm: 2,
+        lg: 4,
+      }}
+      dataSource={result ? result : undefined}
+      locale={{ emptyText: `You haven't made any bookings!` }}
+      pagination={{
+        position: 'top',
+        current: bookingsPage,
+        total: total ? total : undefined,
+        defaultPageSize: limit,
+        hideOnSinglePage: true,
+        showLessItems: true,
+        onChange: (page: number) => setBookingsPage(page),
+      }}
+      renderItem={(userBooking) => {
+        const bookingHistory = (
+          <div className='user-bookings__booking-history'>
+            <div>
+              Check in: <Text strong>{userBooking.checkIn}</Text>
+            </div>
+            <div>
+              Check out: <Text strong>{userBooking.checkOut}</Text>
+            </div>
+          </div>
+        )
+
+        return (
+          <List.Item>
+            {bookingHistory}
+            <ListingCard listing={userBooking.listing} />
+          </List.Item>
+        )
+      }}
+    />
+  )
+
+  const userBookingsElement = userBookingsList && (
+    <div className='user-bookings'>
+      <Title level={4} className='user-bookings__title'>
+        Bookings
+      </Title>
+      <Paragraph>
+        This section highlights the bookings you've made, and the check-in
+        associated with said bookings.
+      </Paragraph>
+      {userBookingsList}
+    </div>
+  )
+
+  return userBookingsElement
 }
