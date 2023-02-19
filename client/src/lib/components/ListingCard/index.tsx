4cf8c234c7b4f0ab5771fbb82627704f7d92375d
Beno Suriano <bjsuriano@gmail.com>
2023-02-18 20:30:29 -0400

    Transform cards into links to specific listing pages.

--- client/src/lib/components/ListingCard/index.tsx
+++ client/src/lib/components/ListingCard/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Card, Typography } from 'antd'
 import { UserOutlined } from '@ant-design/icons'
 import { iconColor, formatListingPrice } from '../../utils'
+import { Link } from 'react-router-dom'
 
 interface Props {
   listing: {
@@ -20,30 +21,32 @@ export const ListingCard = ({ listing }: Props) => {
   const { id, title, image, address, price, numOfGuests } = listing
 
   return (
-    <Card
-      hoverable
-      cover={
-        <div
-          style={{ backgroundImage: `url(${image})` }}
-          className='listing-card__cover-img'
-        />
-      }>
-      <div className='listing-card__details'>
-        <div className='listing-card__description'>
-          <Title className='listing-card__price'>
-            {formatListingPrice(price)}
-            <span>/day</span>
-          </Title>
-          <Text className='listing-card__title'>{title}</Text>
-          <Text ellipsis className='listing-card__address'>
-            {address}
-          </Text>
+    <Link to={`/listing/${id}`}>
+      <Card
+        hoverable
+        cover={
+          <div
+            style={{ backgroundImage: `url(${image})` }}
+            className='listing-card__cover-img'
+          />
+        }>
+        <div className='listing-card__details'>
+          <div className='listing-card__description'>
+            <Title className='listing-card__price'>
+              {formatListingPrice(price)}
+              <span>/day</span>
+            </Title>
+            <Text className='listing-card__title'>{title}</Text>
+            <Text ellipsis className='listing-card__address'>
+              {address}
+            </Text>
+          </div>
+          <div className='listing-card__dimensions listing-card__dimensions--guests'>
+            <UserOutlined type='user' />
+            <Text>{numOfGuests} guests</Text>
+          </div>
         </div>
-        <div className='listing-card__dimensions listing-card__dimensions--guests'>
-          <UserOutlined type='user' />
-          <Text>{numOfGuests} guests</Text>
-        </div>
-      </div>
-    </Card>
+      </Card>
+    </Link>
   )
 }
