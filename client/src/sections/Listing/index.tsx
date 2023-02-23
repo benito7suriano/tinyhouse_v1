aa779b33df33525461491ac2812d2e069dff7fa3
Beno Suriano <bjsuriano@gmail.com>
2023-02-22 21:43:53 -0400

    Listing page query and loading state.

--- client/src/sections/Listing/index.tsx
+++ client/src/sections/Listing/index.tsx
@@ -1,5 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import { LISTING } from '../../lib/graphql/queries'
+import { Layout } from 'antd'
+import { PageSkeleton } from '../../lib/components'
+import {
+  ListingQuery as ListingData,
+  ListingQueryVariables,
+} from '../../gql/graphql'
+import { useParams } from 'react-router-dom'
+
+const { Content } = Layout
+const PAGE_LIMIT = 3
 
 export const Listing = () => {
+  const { id } = useParams()
+  const [bookingsPage, setBookingsPage] = useState(1)
+  const { loading, data, error } = useQuery<ListingData, ListingQueryVariables>(
+    LISTING,
+    {
+      variables: {
+        id: id!,
+        bookingsPage,
+        limit: PAGE_LIMIT,
+      },
+    },
+  )
+
+  if (loading) {
+    return (
+      <Content className='listings'>
+        <PageSkeleton />
+      </Content>
+    )
+  }
+
   return <div>Listing</div>
 }
