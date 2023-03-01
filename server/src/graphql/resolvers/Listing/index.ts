af79cdd199b80f4355bbe3316b3892827ecadc04
Beno Suriano <bjsuriano@gmail.com>
2023-03-01 09:49:05 -0400

    Create temporary resolver for listings in the listingResolvers map.

--- server/src/graphql/resolvers/Listing/index.ts
+++ server/src/graphql/resolvers/Listing/index.ts
@@ -30,6 +30,9 @@ export const listingResolvers: IResolvers = {
         throw new Error(`Failed to query listing: ${error}`)
       }
     },
+    listings: () => {
+      return 'Query.listings'
+    },
   },
   Listing: {
     // Note: A listing document in our database differs from a user document in our database since _id for a listing document is an ObjectId and not a string. This is why we've used the toString() helper to convert the _id value to a string as we resolve the id field for a Listing GraphQL object.
