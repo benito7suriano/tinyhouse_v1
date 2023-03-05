12976ce66640c8f6d7100d9af0a9597913f4a038
Beno Suriano <bjsuriano@gmail.com>
2023-03-04 20:34:27 -0400

    Link HomeHero cards to their respective individual listings page.

--- client/src/sections/Home/components/HomeHero/index.tsx
+++ client/src/sections/Home/components/HomeHero/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card, Col, Input, Row, Typography } from 'antd'
+import { Link } from 'react-router-dom'
 
 import torontoImage from '../../assets/toronto.jpg'
 import dubaiImage from '../../assets/dubai.jpg'
@@ -25,18 +26,28 @@ export const HomeHero = () => {
       </div>
       <Row gutter={12} className='home-hero__cards'>
         <Col xs={12} md={6}>
-          <Card cover={<img alt='Toronto' src={torontoImage} />}>Toronto</Card>
+          <Link to={'/listings/toronto'}>
+            <Card cover={<img alt='Toronto' src={torontoImage} />}>
+              Toronto
+            </Card>
+          </Link>
         </Col>
         <Col xs={12} md={6}>
-          <Card cover={<img alt='Dubai' src={dubaiImage} />}>Dubai</Card>
+          <Link to={'/listings/dubai'}>
+            <Card cover={<img alt='Dubai' src={dubaiImage} />}>Dubai</Card>
+          </Link>
         </Col>
         <Col xs={0} md={6}>
-          <Card cover={<img alt='Los Angeles' src={losAngelesImage} />}>
-            Los Angeles
-          </Card>
+          <Link to={'/listings/los%20angeles'}>
+            <Card cover={<img alt='Los Angeles' src={losAngelesImage} />}>
+              Los Angeles
+            </Card>
+          </Link>
         </Col>
         <Col xs={0} md={6}>
-          <Card cover={<img alt='London' src={londonImage} />}>London</Card>
+          <Link to={'/listings/london'}>
+            <Card cover={<img alt='London' src={londonImage} />}>London</Card>
+          </Link>
         </Col>
       </Row>
     </div>
