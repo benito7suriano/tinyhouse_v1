cc4cf9cac3d42065de00f7e47aaa7ce411d5026c
Beno Suriano <bjsuriano@gmail.com>
2023-03-19 17:50:06 -0400

    Update the Google API to geocode a specific address.

--- server/src/lib/api/Google.ts
+++ server/src/lib/api/Google.ts
@@ -1,10 +1,44 @@
 import { google } from 'googleapis'
-import { Client } from '@googlemaps/google-maps-services-js'
+import {
+  Client,
+  GeocodeRequest,
+  AddressComponent,
+  PlaceType2,
+  PlaceType1,
+} from '@googlemaps/google-maps-services-js'
+
 const auth = new google.auth.OAuth2(
   process.env.G_CLIENT_ID,
   process.env.G_CLIENT_SECRET,
   `${process.env.PUBLIC_URL}/login`,
 )
+
+const maps = new Client({})
+
+const parseAddress = (addressComponents: AddressComponent[]) => {
+  let country = null
+  let admin = null
+  let city = null
+
+  for (const component of addressComponents) {
+    if (component.types.includes(PlaceType2.country)) {
+      country = component.long_name
+    }
+
+    if (component.types.includes(PlaceType2.administrative_area_level_1)) {
+      admin = component.long_name
+    }
+
+    if (
+      component.types.includes(PlaceType2.locality) ||
+      component.types.includes(PlaceType2.postal_town)
+    ) {
+      city = component.long_name
+    }
+
+    return { country, admin, city }
+  }
+}
 
 export const Google = {
   authUrl: auth.generateAuthUrl({
@@ -29,6 +63,17 @@ export const Google = {
     return { user: data }
   },
   geocode: async (address: string) => {
-    // boilerplate
+    const req: GeocodeRequest = {
+      params: {
+        address: address,
+        key: process.env.GOOGLE_MAPS_API_KEY as string,
+      },
+    }
+    const res = await maps.geocode(req)
+    if (res.status < 200 || res.status > 299) {
+      throw new Error('Failed to geocode address')
+    }
+
+    return parseAddress(res.data.results[0].address_components)
   },
 }
