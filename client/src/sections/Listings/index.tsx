7289e25c79aca59be6c0ce7eec98f8c15371dbf1
Beno Suriano <bjsuriano@gmail.com>
2023-03-23 16:05:12 -0400

    Query for listings from Listings component.

--- client/src/sections/Listings/index.tsx
+++ client/src/sections/Listings/index.tsx
@@ -1,5 +1,28 @@
 import React from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import { Layout, List } from 'antd'
+import { ListingCard } from '../../lib/components'
+import { LISTINGS } from '../../lib/graphql/queries'
+import {
+  ListingsQuery as ListingsData,
+  ListingsQueryVariables,
+} from '../../gql/graphql'
+import { ListingsFilter } from '../../gql/graphql'
+
+const { Content } = Layout
+const PAGE_LIMIT = 8
 
 export const Listings = () => {
+  const { data } = useQuery<ListingsData, ListingsQueryVariables>(LISTINGS, {
+    variables: {
+      filter: ListingsFilter.PriceLowToHigh,
+      limit: PAGE_LIMIT,
+      page: 1,
+    },
+  })
+
+  // When data is available from our query, we'll look to obtain the listings field from data and assign it to a listings constant.
+  const listings = data ? data.listings : null
+
   return <div>Listings</div>
 }
