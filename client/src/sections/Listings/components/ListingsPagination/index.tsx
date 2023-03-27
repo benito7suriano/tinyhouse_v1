e7f9cffd1adc7204da3b93c41c1ae6094dbcdf01
Beno Suriano <bjsuriano@gmail.com>
2023-03-27 17:43:34 -0400

    Define props for ListingsPagination component.

--- client/src/sections/Listings/components/ListingsPagination/index.tsx
+++ client/src/sections/Listings/components/ListingsPagination/index.tsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import { Pagination } from 'antd'
+
+interface Props {
+  total: number
+  page: number
+  limit: number
+  setPage: (page: number) => void
+}
 
 export const ListingsPagination = () => {
   return <div>ListingsPagination</div>
